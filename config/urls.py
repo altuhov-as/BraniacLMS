1dcb6850e56cbd767aaeaac5c156884445af9ec5
altuhov_as <altuhov_as@mail.ru>
2022-05-19 00:44:59 +0300

    lesson_2 Code style fix

--- config/urls.py
+++ config/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from mainapp import views
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(url="mainapp/")),
-    path('mainapp/', include("mainapp.urls")),
+    path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url="mainapp/")),
+    path("mainapp/", include("mainapp.urls")),
 ]
